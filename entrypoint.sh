a7a3fad9dc67e507fd4b5d5bdc993eed1b579f9c
Torrentov <44697363+Torrentov@users.noreply.github.com>
2025-03-20 11:44:23 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Waiting for the database"
-until nc -z db 5432; do
-  echo "Database unavailable, retry in 5 seconds"
-  sleep 5
-done
-echo "Database available"
-
 alembic upgrade head
 
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000
