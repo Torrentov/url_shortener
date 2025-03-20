8811163fcad9233d9ca402ec9d840682af2f519b
Torrentov <44697363+Torrentov@users.noreply.github.com>
2025-03-20 10:26:49 +0300

    add database wait

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+set -e
+
+echo "Waiting for the database"
+until nc -z db 5432; do
+  echo "Database unavailable, retry in 5 seconds"
+  sleep 5
+done
+echo "Database available"
 
 alembic upgrade head
 
